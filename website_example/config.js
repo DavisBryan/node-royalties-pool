579c78013618f848947e83489e8d1945cb139fc8
Rob-Otman <britzst9617@gmail.com>
2017-08-03 00:53:58 -0600

    block explorer api fixes
    added functions to instert block and transaction id's mid url for xryexplorer.info

--- website_example/config.js
+++ website_example/config.js
@@ -1,6 +1,6 @@
 var api = "http://cryppit.com:8117";
 
-var coinUnits = 1000000000000;
+var coinUnits = 1000000;
 
 var poolHost = "cryppit.com";
 
@@ -8,10 +8,10 @@ var irc = "irc.freenode.net/#monero-pools";
 
 var email = "support@cryppit.com";
 
-var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+var cryptonatorWidget = ["XRY-BTC", "XRY-USD", "XRY-EUR", "XRY-GBP"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/xmr/block/";
+var blockchainExplorer = "http://xryexplorer.info/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://chainradar.com/xmr/transaction/";
+var transactionExplorer = "http://xryexplorer.info/?hash={id}#blockchain_transaction";
